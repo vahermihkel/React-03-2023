211ba188d35d6156588e9b0f0677e1b63af9353b
mihkelvaher <mihkel.vaher@industry62.com>
2023-03-06 20:36:10 +0200

    Reacti algus

---
+++ veebipood/src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img className="pilt" src="https://upload.wikimedia.org/wikipedia/en/9/99/Nobe_GT100.jpg" alt="" />
+      <button className="nupp">Nupp</button>
+      {/* <p></p>
+      <div>Tekst</div> */}
+      {/* <span></span> */}
+      {/* <img src="https://upload.wikimedia.org/wikipedia/en/9/99/Nobe_GT100.jpg" alt=""></img>
+      <input type="text"></input> */}
+
+    </div>
+  );
+}
+
+export default App;
